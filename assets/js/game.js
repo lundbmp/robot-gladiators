5f22b5e34dc8c019c879fc53367ef10ec65515ef
lundbmp <m.lundberg008@gmail.com>
2022-05-05 17:59:46 -0500

    commiting for safety

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,6 +71,7 @@ for(var i = 0; i < enemyNames.length; i++) {
         window.alert("You have lost your robot in battle! Game Over!");
         break;
     }
+    
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
