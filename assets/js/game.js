f14680419afe308dd33453233d40eefa8de872d8
lundbmp <m.lundberg008@gmail.com>
2022-05-05 13:35:33 -0500

    added multiple enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,11 @@ var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyRobot) {
     window.alert("Welcome to Robot Gladiatiors!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -18,20 +18,20 @@ var fight = function() {
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
         console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        playerName + " attacked " + enemyRobot + ". " + enemyRobot + " now has " + enemyHealth + " health remaining."
         );
     
         // check enemy's health
         if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died!");
+        window.alert(enemyRobot + " has died!");
         } else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        window.alert(enemyRobot + " still has " + enemyHealth + " health left.");
         }
     
         // remove player's health by subtracting the amount set in the enemyAttack variable
         playerHealth = playerHealth - enemyAttack;
         console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+            enemyRobot + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
     
         // check player's health
@@ -51,7 +51,7 @@ var fight = function() {
         }
           // if no (false), ask question again by running fight() again
         else {
-            fight();
+            fight(enemyRobot);
         }
     } 
     else {
@@ -59,5 +59,6 @@ var fight = function() {
     }
 }
 
-
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
