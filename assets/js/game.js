b19ad09d2f1d922fcc157a0389e752bbbc04f550
lundbmp <m.lundberg008@gmail.com>
2022-05-08 22:10:39 -0500

    adjusted shop input

--- assets/js/game.js
+++ assets/js/game.js
@@ -192,11 +192,13 @@ var endGame = function() {
 var shop = function() {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
+  shopOptionPrompt = parseInt(shopOptionPrompt);
+
   // use switch to carry out action
-  switch (shopOptionPrompt.toUpperCase()) {
-    case "REFILL":
+  switch (shopOptionPrompt) {
+    case 1:
       if (playerInfo.money >= 7) {
         window.alert("Refilling player's health by 20 for 7 dollars.");
     
@@ -208,7 +210,7 @@ var shop = function() {
       }
     
       break;
-    case "UPGRADE":
+    case 2:
       if (playerInfo.money >= 7) {
         window.alert("Upgrading player's attack by 6 for 7 dollars.");
     
@@ -220,7 +222,7 @@ var shop = function() {
       }
     
       break;
-    case "LEAVE":
+    case 3:
       window.alert("Leaving the store.");
 
       // do nothing, so function will end
